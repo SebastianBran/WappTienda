71089ca71bbc94cc6495dfbf1fb11543812df071
Sebastian Bran <esbran@avatar-global.com>
2024-12-05 22:32:02 -0500

    development: home first version

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,17 +1,44 @@
-import { useNavigate } from 'react-router-dom'
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Home</h1>
-      <div>
-        <button onClick={() => navigate('/login')}>Login</button>
-        <button onClick={() => navigate('/register')}>Registrarse</button>
-      </div>
+    <div className="flex flex-col bg-white h-screen">
+      <header className="py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center">
+            <span className='text-xl mr-4 font-bold'>WappTienda</span>
+          </div>
+          <div className="flex gap-4">
+            <Button variant="ghost" onClick={() => navigate('/login')}>
+              Iniciar sesión
+            </Button>
+            <Button onClick={() => navigate('/register')}>
+              Registrarse
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      <main className='h-full'>
+        <section className="bg-gray-100 pb-16 pt-36 h-full bg-gradient-to-t from-teal-100 to-white">
+          <div className="container mx-auto h-full">
+            <div className="max-w-3xl mx-auto text-center">
+              <h1 className="text-6xl font-bold mb-4">Crea Ecommerce para WhatsApp</h1>
+              <p className="text-gray-600 my-8">
+                Crea tu tienda online y simplifica tus ventas con WhatsApp. Con WappTienda puedes crear tu tienda en minutos y empezar a vender tus productos.
+              </p>
+              <Button size="lg" onClick={() => navigate('/register')}>
+                Comienza ahora
+              </Button>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default Home;

e5d25fb1fcfdb05721084426acc976a00ff6e5ab
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-05-03 13:54:31 -0500

    feat(http): implement axios instance with request and response interceptors for authentication

--- src/api/services/http-common.ts
+++ src/api/services/http-common.ts
@@ -1,6 +1,7 @@
+import { getItem } from "@/lib/local-storage";
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-type": "application/json",
@@ -9,3 +10,61 @@ export default axios.create({
     "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   },
 });
+
+const excludedRequestInterceptorPaths = [
+  "/auth/login",
+]
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const requestPath = config.url;
+
+    if (
+      excludedRequestInterceptorPaths.some((path) =>
+        requestPath?.includes(path),
+      )
+    ) {
+      return config;
+    }
+
+    const accessToken = getItem("accessToken");
+    if (config.headers) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  },
+);
+
+const excludedResponseInterceptorPaths = [
+  "/auth/login",
+  "/auth/refresh",
+  "/auth/logout",
+];
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const requestPath = error.config?.url;
+
+    if (
+      excludedResponseInterceptorPaths.some((path) =>
+        requestPath?.includes(path),
+      )
+    ) {
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;

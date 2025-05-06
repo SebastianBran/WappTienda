ce300859fccc5e1319bf27850fd1ccf6f467d71a
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-05-05 23:14:06 -0500

    feat(orders): implement OrderService with CRUD operations for orders

---
+++ src/api/services/orders.service.ts
@@ -0,0 +1,26 @@
+import { Order } from "@/types/orders";
+import http from "./http-common";
+
+class OrderService {
+  async getAll(): Promise<Order[]> {
+    // TODO: Implement pagination
+    const response = await http.get<Order[]>("/orders");
+    return response.data;
+  }
+
+  async getById(id: number): Promise<Order> {
+    const response = await http.get<Order>(`/orders/${id}`);
+    return response.data;
+  }
+
+  async update(id: number, order: Partial<Order>): Promise<Order> {
+    const response = await http.patch<Order>(`/orders/${id}`, order);
+    return response.data;
+  }
+
+  async delete(id: number) {
+    await http.delete(`/orders/${id}`);
+  }
+}
+
+export default new OrderService();

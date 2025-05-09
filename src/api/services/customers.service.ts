125547283c900884606b97a7e342b8495b7629d2
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-05-08 23:39:17 -0500

    feat(customers): implement CustomersService with CRUD operations

---
+++ src/api/services/customers.service.ts
@@ -0,0 +1,26 @@
+import { Customer } from "@/types/customers";
+import http from "./http-common";
+
+class CustomersService {
+  async getAll() {
+    // TODO: Implement pagination
+    const response = await http.get("/customers");
+    return response.data;
+  }
+
+  async getById(id: number): Promise<Customer> {
+    const response = await http.get(`/customers/${id}`);
+    return response.data;
+  }
+
+  async update(id: number, customer: Partial<Customer>): Promise<Customer> {
+    const response = await http.patch(`/customers/${id}`, customer);
+    return response.data;
+  }
+
+  async delete(id: number) {
+    await http.delete(`/customers/${id}`);
+  }
+}
+
+export default new CustomersService();

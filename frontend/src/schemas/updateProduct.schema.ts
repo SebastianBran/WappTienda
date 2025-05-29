8ff7fdf90d2b904ebc9032d2afbce13575bc7df1
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-05-28 22:21:36 -0500

    feat(schema): update updateProductSchema to allow optional totalInventory and improve validation logic

--- frontend/src/schemas/updateProduct.schema.ts
+++ frontend/src/schemas/updateProduct.schema.ts
@@ -1,17 +1,31 @@
-import { ProductType } from '@/types/products';
-import { z } from 'zod';
+import { ProductType } from "@/types/products";
+import { z } from "zod";
 
-const updateProductSchema = z.object({
-  name: z.string().nonempty(),
-  sku: z.string().optional(),
-  visible: z.boolean().optional(),
-  type: z.nativeEnum(ProductType),
-  description: z.string().optional(),
-  trackInventory: z.boolean(),
-  totalInventory: z.number().int().positive().optional(),
-  salesPrice: z.number().positive().optional(),
-  price: z.number().positive(),
-});
+const updateProductSchema = z
+  .object({
+    name: z.string().nonempty(),
+    sku: z.string().optional(),
+    visible: z.boolean().optional(),
+    type: z.nativeEnum(ProductType),
+    description: z.string().optional(),
+    trackInventory: z.boolean(),
+    totalInventory: z.number().int().optional(),
+    salesPrice: z.number().positive().optional(),
+    price: z.number().positive(),
+  })
+  .refine(
+    ({ trackInventory, totalInventory }) => {
+      if (trackInventory && (!totalInventory || totalInventory < 0)) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "El inventario total es requerido",
+      path: ["totalInventory"],
+    },
+  );
 
 export type UpdateProductSchema = z.infer<typeof updateProductSchema>;
 

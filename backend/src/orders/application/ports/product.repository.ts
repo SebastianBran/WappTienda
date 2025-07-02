51eb2be14543bead811895abf7c1c0a70c16a5ea
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-07-01 22:12:55 -0500

    refactor(orders): remove unused product and customer entities, repository, and factory

--- backend/src/orders/application/ports/product.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Product } from 'src/orders/domain/entities/product.entity';
-
-export interface ProductRepository {
-  findById(id: number): Promise<Product | null>;
-}
--- backend/src/orders/domain/entities/product-type.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum ProductType {
-  VIRTUAL = 'VIRTUAL',
-  PHYSICAL = 'PHYSICAL',
-}

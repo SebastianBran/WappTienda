7dd4b51d660d02e5625850c264b29971fc3196c0
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-07-03 00:12:41 -0500

    fix(users): ensure BCRYPT_SALT_ROUNDS is parsed as a number in BcryptPasswordService

--- backend/src/users/infrastructure/adapters/bcrypt-password.service.impl.ts
+++ backend/src/users/infrastructure/adapters/bcrypt-password.service.impl.ts
@@ -1,12 +1,14 @@
 import { PasswordService } from 'src/users/application/ports/password.service';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class BcryptPasswordService implements PasswordService {
   constructor(private readonly configService: ConfigService) {}
 
   async hashPassword(password: string): Promise<string> {
-    const saltRounds = this.configService.get<number>('BCRYPT_SALT_ROUNDS', 10);
+    const saltRounds = Number(this.configService.get('BCRYPT_SALT_ROUNDS', 10));
     return await bcrypt.hash(password, saltRounds);
   }
 }

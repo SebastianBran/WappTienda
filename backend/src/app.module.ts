4b15e333e8d2cefa45945e721b0924ec81336612
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-07-03 21:23:20 -0500

    refactor(app): remove AppController and AppService as they are no longer needed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -38,8 +36,6 @@ import { StatisticsModule } from './statistics/statistics.module';
     ConfigurationsModule,
     StatisticsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
